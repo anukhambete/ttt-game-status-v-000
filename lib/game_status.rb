210a28cdaff08b1ab6065f84c448637691182185
Anuradha Khambete <anukhambete@gmail.com>
2018-04-03 03:31:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,7 +41,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if full?(board) == true || won?(board) == true || draw?(board) == true
+  if won?(board) == true || draw?(board) == true
   true
   else
   false
